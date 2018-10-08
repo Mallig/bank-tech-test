256a93a95ce570a9e6c12248c298d6a6bd9654a7
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-08 16:20:23 +0100

    test: statement stores transaction

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,3 +1,17 @@
 describe('Statement', function() {
+  var statement
+  var mockTransaction
 
+  beforeEach(function() {
+    statement = new Statement()
+    mockTransaction = { type: function() { return 'deposit' },
+                      amount: function() { return 50 },
+                        date: function() { return "Mon Oct 08 2018 15:10:40 GMT+0100" }
+                    }
+  })
+
+  it('should store transactions', function() {
+    statement.store(mockTransaction)
+    expect(statement.transactions).toEqual([mockTransaction])
+  })
 })
