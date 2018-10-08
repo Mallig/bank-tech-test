c5cd81da70b3bac59ac3fbec7c176f1e35e6394f
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-08 12:14:31 +0100

    test: account balance existence fails

---
+++ spec/AccountSpec.js
@@ -0,0 +1,11 @@
+describe('Account', function() {
+  var account
+
+  beforeEach(function() {
+    account = new Account()
+  })
+
+  it('should have a balance', function() {
+    expect(account.balance()).toBe
+  })
+})
