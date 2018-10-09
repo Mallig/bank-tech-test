ccc0eebe7faf40748bef1bf4e3df03defe4a8a8a
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-09 09:13:29 +0100

    test: statement display test passes

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -4,14 +4,22 @@ describe('Statement', function() {
 
   beforeEach(function() {
     statement = new Statement()
-    mockTransaction = { type: function() { return 'deposit' },
-                      amount: function() { return 50 },
-                        date: function() { return "Mon Oct 08 2018 15:10:40 GMT+0100" }
+    mockTransaction = { // type: function() { return 'deposit' },
+    //                   amount: function() { return 50 },
+    //                  balance: function() { return 50 },
+    //                     date: function() { return "14/01/2012" },
+                    _display: function() { return '14/01/2012 || 50 || || 50' }
                     }
   })
 
   it('should store transactions', function() {
     statement.store(mockTransaction)
     expect(statement.transactions).toContain(mockTransaction)
+  })
+  
+  it('can print a statement', function() {
+    statement.store(mockTransaction)
+    var printout = statement.display()
+    expect(printout).toEqual('date || credit || debit || balance\n14/01/2012 || 50 || || 50')
   })
 })
